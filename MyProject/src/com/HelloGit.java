6e1beef9534d6728c75c352f849c8fca1f9e0f43
BhuvaneshMani <bhuvaneshmece@gmail.com>
2017-02-12 11:39:37 -0500

    Fixed a change

--- MyProject/src/com/HelloGit.java
+++ MyProject/src/com/HelloGit.java
@@ -1,9 +1,14 @@
 package com;
 
 public class HelloGit {
+	String newVariable = "";
 	
 	public static void main(String[] args) {
 		System.out.println("hello world");
+	}
+	
+	public void function2(String check){
+		System.out.println("This is simply awesome..");
 	}
 
 }
