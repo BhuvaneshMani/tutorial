0760e966bb9bf6984e37b8f708eb4ab71cbf109e
BhuvaneshMani <bhuvaneshmece@gmail.com>
2017-02-12 11:40:13 -0500

    done with changes

--- MyProject/src/com/HelloGit.java
+++ MyProject/src/com/HelloGit.java
@@ -5,5 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
+	
+	public void addedNewFile(){
+		System.out.println("Checking out integration");
+	}
 
 }
