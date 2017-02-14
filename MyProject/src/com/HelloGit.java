a938935fbf165d63c148e0247ecde2c9ee070ca7
manibhuvanesh <santhi.mani1965@gmail.com>
2017-02-14 00:37:00 -0500

    fall to 1

--- MyProject/src/com/HelloGit.java
+++ MyProject/src/com/HelloGit.java
@@ -6,14 +6,5 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
-	
-	public void addedNewFile(){
-		System.out.println("Checking out integration");
-}
-	public void function2(String check){
-		System.out.println("This is simply awesome..");
-	}
-	
-	//Added new line - check
 
 }
